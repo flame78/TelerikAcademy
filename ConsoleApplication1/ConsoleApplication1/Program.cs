08a71da55df9d9e5af470edb76ef3add47efe587
flame78 <flame78@gmail.com>
2014-02-08 08:52:31 +0200

    1

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -5,7 +5,7 @@
         static void Main()
         {
             //MySqlTransfer.CreateMyDbTbl("localhost", "root", "toor");
-            MySqlTransfer.DatFile("localhost", "test", "tbl", "root", "toor", @"..\..\..\2014-02-04.txt");
+            MySqlTransfer.DatFile("localhost", "test", "tbl", "root", "toor", @"..\..\2014-02-04.dat");
         }
     }
 
