a82c6a4cc1133bef5c9587adc917eba4f4dcf717
flame78 <flame78@gmail.com>
2014-02-17 16:59:12 +0200

    111

---
+++ OOP/ObjectOrientedProgrammingPrinciplesPartI/ObjectOrientedProgrammingPrinciplesPartI/People.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ObjectOrientedProgrammingPrinciplesPartI
+{
+    public class People
+    {
+        private string name;
+
+        public People(string name)
+        {
+            this.name = name;
+        }
+    
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+        }
+    }
+}
