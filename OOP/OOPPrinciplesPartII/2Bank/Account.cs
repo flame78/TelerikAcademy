c9fc53abf8cb2726cce1aee3e2557e0b06e41f25
flame78 <flame78@gmail.com>
2014-02-20 07:51:17 +0200

    1

---
+++ OOP/OOPPrinciplesPartII/2Bank/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2Bank
+{
+    class Account
+    {
+        private Customer customer;
+        private decimal balance;
+        private decimal interestRate;
+
+        //decimal Balance()
+        //{ 
+        //    get; { return this.balance; }
+        //    set; { this.balance = value; }
+        //}
+        public void Deposit(decimal money)
+        { 
+
+        }
+        public virtual decimal CalculateInterest(decimal numberOfMonths)
+        {
+            return numberOfMonths * this.interestRate;
+        }
+    }
+}
