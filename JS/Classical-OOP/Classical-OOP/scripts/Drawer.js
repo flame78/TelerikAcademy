be442adbceaaf135c2b8386909ba8d9a14c529ef
flame78 <flame78@gmail.com>
2014-06-29 15:23:13 +0300

    canvasDrawer

---
+++ JS/Classical-OOP/Classical-OOP/scripts/Drawer.js
@@ -0,0 +1,29 @@
+﻿var drawer = function drawer(canvasID) {
+
+    var container = document.getElementById(canvasID);
+    var ctx = container.getContext("2d");
+
+    function rect(x, y, width, height) {
+        ctx.beginPath();
+        ctx.rect(x, y, width, height);
+        ctx.fill();
+    }    function circle(centerX, centerY, radius) {
+        ctx.beginPath();
+        ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+
+    function line(fromX, fromY, toX, toY) {
+        ctx.beginPath();
+        ctx.lineWidth = 5
+        ctx.moveTo(fromX, fromY);
+        ctx.lineTo(toX, toY);
+        ctx.stroke();
+    }
+
+    return {
+        rect: rect,
+        circle: circle,
+        line: line
+    }
+};
