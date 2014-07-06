564e1cb73c0615fcd1e1d8150cade87257ccfa7e
flame78 <flame78@gmail.com>
2014-07-06 19:01:53 +0300

    rewriten

--- JS/JS-OOP-Exam-Preparation-task2/JS-OOP-Exam-Preparation-task2/scripts/courses/course.js
+++ JS/JS-OOP-Exam-Preparation-task2/JS-OOP-Exam-Preparation-task2/scripts/courses/course.js
@@ -1,84 +1,145 @@
 /// <reference path="../libs/requirejs/require.js" />
 define(['courses/student'], function (Student) {
 
-    function Course(title, totalScoreFunction) {
-        validateTitle(title);
-        this._title = title;
-        validateScoreFunction(totalScoreFunction);
-        this._totalScoreFunction = totalScoreFunction;
-        this._students = [];
-    }
+    var Course;
 
-    Course.prototype = {
-        addStudent: addStudent,
-        calculateResults: calculateResults,
-        getTopStudentsByExam: getTopStudentsByExam,
-        getTopStudentsByTotalScore: getTopStudentsByTotalScore
+    Course = (function () {
 
-    }
-
-    function addStudent(student) {
-        if (!(student instanceof Student)) {
-            throw new TypeError('student must be instanceof Student');
+        function Course(title, totalScoreFormula) {
+            this._title = title;
+            this._totalScoreFormula = totalScoreFormula;
+            this._students = []
         }
-        this._students.push(student);
-    }
 
-    function calculateResults() {
-        for (var i = 0; i < this._students.length; i++) {
-            this._students[i].courseResult = this._totalScoreFunction(this._students[i]);
+        function sortBy(property) {
+            var sortedStudents = this._students.slice(0);
+
+            sortedStudents.sort(function (a, b) {
+                return b[property] - a[property]
+            });
+
+            return sortedStudents;
+
         }
-    }
 
-    function getTopStudentsByExam(number) {
+        Course.prototype = {
 
-        this._students.sort(function (a, b) {
-            return b.exam - a.exam;
-        });
+            addStudent: function (student) {
 
-        return getCopyOfFirst(this, number);
-    }
+                if (!(student instanceof Student)) {
+                    throw new TypeError('Course.addStudent can only work with instance of Student');
+                }
 
-    function getTopStudentsByTotalScore(number) {
+                this._students.push(student);
 
-        this._students.sort(function (a, b) {
-            return b.courseResult - a.courseResult;
-        })
+                return this;
+            },
 
-        return getCopyOfFirst(this, number);
-    }
+            calculateResults: function () {
+                var self = this;
+                this._students.forEach(function (student) {
+                    student.totalScore = self._totalScoreFormula(student);
+                });
+            },
+
+            getTopStudentsByExam: function (count) {
+                return sortBy.call(this, 'exam').slice(0, count);
+            },
+
+            getTopStudentsByTotalScore: function (count) {
+                return sortBy.call(this, 'totalScore').slice(0, count);
+            }
+        }
+
+        return Course;
+    }());
 
     return Course;
-
-    function validateTitle(title) {
-
-        if (typeof (title) != 'string') {
-            throw new TypeError('The title must be a string');
-        }
-
-        if (!title.trim()) {
-            throw new Error('Title cant be empty or whitespace')
-        }
-    }
-
-    function validateScoreFunction(totalScoreFunction) {
-
-        if (typeof (totalScoreFunction) != 'function') {
-            throw new TypeError('totalScoreFunction must be a function');
-        }
-    }
-
-    function getCopyOfFirst(course, number) {
-
-        if (number > course._students.length) {
-            number = course._students.length
-        }
-
-        var result = [];
-        for (var i = 0; i < number; i++) {
-            result.push(JSON.parse(JSON.stringify(course._students[i])));
-        }
-
-        return result;
-    }
 });
+
+// first time writen
+
+///// <reference path="../libs/requirejs/require.js" />
+//define(['courses/student'], function (Student) {
+
+//    function Course(title, totalScoreFunction) {
+//        validateTitle(title);
+//        this._title = title;
+//        validateScoreFunction(totalScoreFunction);
+//        this._totalScoreFunction = totalScoreFunction;
+//        this._students = [];
+//    }
+
+//    Course.prototype = {
+//        addStudent: addStudent,
+//        calculateResults: calculateResults,
+//        getTopStudentsByExam: getTopStudentsByExam,
+//        getTopStudentsByTotalScore: getTopStudentsByTotalScore
+
+//    }
+
+//    function addStudent(student) {
+//        if (!(student instanceof Student)) {
+//            throw new TypeError('student must be instanceof Student');
+//        }
+//        this._students.push(student);
+//    }
+
+//    function calculateResults() {
+//        for (var i = 0; i < this._students.length; i++) {
+//            this._students[i].courseResult = this._totalScoreFunction(this._students[i]);
+//        }
+//    }
+
+//    function getTopStudentsByExam(number) {
+
+//        this._students.sort(function (a, b) {
+//            return b.exam - a.exam;
+//        });
+
+//        return getCopyOfFirst(this, number);
+//    }
+
+//    function getTopStudentsByTotalScore(number) {
+
+//        this._students.sort(function (a, b) {
+//            return b.courseResult - a.courseResult;
+//        })
+
+//        return getCopyOfFirst(this, number);
+//    }
+
+//    return Course;
+
+//    function validateTitle(title) {
+
+//        if (typeof (title) != 'string') {
+//            throw new TypeError('The title must be a string');
+//        }
+
+//        if (!title.trim()) {
+//            throw new Error('Title cant be empty or whitespace')
+//        }
+//    }
+
+//    function validateScoreFunction(totalScoreFunction) {
+
+//        if (typeof (totalScoreFunction) != 'function') {
+//            throw new TypeError('totalScoreFunction must be a function');
+//        }
+//    }
+
+//    function getCopyOfFirst(course, number) {
+
+//        if (number > course._students.length) {
+//            number = course._students.length
+//        }
+
+//        var result = [];
+//        for (var i = 0; i < number; i++) {
+//            result.push(JSON.parse(JSON.stringify(course._students[i])));
+//        }
+
+//        return result;
+//    }
+//});
--- JS/JS-OOP-Exam-Preparation-task2/JS-OOP-Exam-Preparation-task2/scripts/courses/student.js
+++ JS/JS-OOP-Exam-Preparation-task2/JS-OOP-Exam-Preparation-task2/scripts/courses/student.js
@@ -1,46 +1,71 @@
 /// <reference path="../libs/requirejs/require.js" />
-define(function () {
+define([''], function () {
+    'use strict';
+    var Student;
 
-    NegativeScoreError.prototype = new Error();
-    
-    function Student(object) {
-        validateName(object.name);
-        this.name = object.name;
-        validateScore(object.exam);
-        this.exam = object.exam;
-        validateScore(object.homework);
-        this.homework = object.homework;
-        validateScore(object.attendance);
-        this.attendance = object.attendance;
-        validateScore(object.teamwork);
-        this.teamwork = object.teamwork;
-        validateScore(object.bonus);
-        this.bonus = object.bonus;
-    }
+    Student = (function () {
 
-    function NegativeScoreError() {
-        this.message = 'Score cant be negative'
-    }
- 
+        function Student(data) {
+            this.name = data.name;
+            this.exam = data.exam;
+            this.homework = data.homework;
+            this.attendance = data.attendance;
+            this.teamwork = data.teamwork;
+            this.bonus = data.bonus;
+            this.totalScore = 'Not Calculated';
+        }
+
+        return Student;
+    }());
+
     return Student;
-
-    function validateScore(score) {
-
-        if (typeof (score) != 'number') {
-            throw new Error('The score be a number');
-        }
-        if (score < 0) {
-            throw new NegativeScoreError();
-        }
-    }
-
-    function validateName(name) {
-        if (typeof (name) != 'string') {
-            throw new TypeError('The name must be a string');
-        }
-        if (!name.trim()) {
-            throw new Error('Name cant be empty or whitespace')
-        }
-    }
-
 });
+
+// first time write
+
+/// <reference path="../libs/requirejs/require.js" />
+//define(function () {
+
+//    NegativeScoreError.prototype = new Error();
+    
+//    function Student(object) {
+//        validateName(object.name);
+//        this.name = object.name;
+//        validateScore(object.exam);
+//        this.exam = object.exam;
+//        validateScore(object.homework);
+//        this.homework = object.homework;
+//        validateScore(object.attendance);
+//        this.attendance = object.attendance;
+//        validateScore(object.teamwork);
+//        this.teamwork = object.teamwork;
+//        validateScore(object.bonus);
+//        this.bonus = object.bonus;
+//    }
+
+//    function NegativeScoreError() {
+//        this.message = 'Score cant be negative'
+//    }
+ 
+//    return Student;
+
+//    function validateScore(score) {
+
+//        if (typeof (score) != 'number') {
+//            throw new Error('The score be a number');
+//        }
+//        if (score < 0) {
+//            throw new NegativeScoreError();
+//        }
+//    }
+
+//    function validateName(name) {
+//        if (typeof (name) != 'string') {
+//            throw new TypeError('The name must be a string');
+//        }
+//        if (!name.trim()) {
+//            throw new Error('Name cant be empty or whitespace')
+//        }
+//    }
+
+//});
